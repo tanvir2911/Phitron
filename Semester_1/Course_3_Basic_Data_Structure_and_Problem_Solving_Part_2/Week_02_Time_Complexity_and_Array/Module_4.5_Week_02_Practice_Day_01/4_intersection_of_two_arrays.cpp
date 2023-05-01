219a9737dd30ba63fa75c0bc6198ade1c3802a3c
Md Mozammel Hossain <hossainmozammel27@gmail.com>
2023-05-01 15:22:55 +0600

    module 4.5 problem 4 added

--- Semester_1/Course_3_Basic_Data_Structure_and_Problem_Solving_Part_2/Week_02_Time_Complexity_and_Array/Module_4.5_Week_02_Practice_Day_01/4_intersection_of_two_arrays.cpp
+++ Semester_1/Course_3_Basic_Data_Structure_and_Problem_Solving_Part_2/Week_02_Time_Complexity_and_Array/Module_4.5_Week_02_Practice_Day_01/4_intersection_of_two_arrays.cpp
@@ -1,1 +1,39 @@
+#include <bits/stdc++.h>
 
+using namespace std;
+/*
+time complexity = O(n*m) = O(n^2)
+memory complexity = O(n) = O(n)
+*/
+int main()
+{
+    int n,m;
+    cin>>n;
+    vector<int>ara1(n);
+    for(int i=0;i<n;i++)
+        cin>>ara1[i];
+    cin>>m;
+    vector<int>ara2(m);
+    for(int i=0;i<m;i++)
+        cin>>ara2[i];
+
+    vector<int>intersection;
+
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            if(ara1[i]==ara2[j])
+            {
+                intersection.push_back(ara1[i]);
+                break;
+            }
+        }
+    }
+
+    for(int i=0;i<intersection.size();i++)
+        cout<<intersection[i]<<" ";
+    cout<<"\n";
+
+    return 0;
+}
