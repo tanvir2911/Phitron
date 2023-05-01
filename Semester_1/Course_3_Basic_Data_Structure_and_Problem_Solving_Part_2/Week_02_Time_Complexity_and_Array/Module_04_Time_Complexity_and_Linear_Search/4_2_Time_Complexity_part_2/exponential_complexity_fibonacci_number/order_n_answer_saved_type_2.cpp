1cf5e0485554f897db62c75fe6d5e2bb9594a42b
Md Mozammel Hossain <hossainmozammel27@gmail.com>
2023-05-01 12:31:23 +0600

    Order n for fibonnacci number added

---
+++ Semester_1/Course_3_Basic_Data_Structure_and_Problem_Solving_Part_2/Week_02_Time_Complexity_and_Array/Module_04_Time_Complexity_and_Linear_Search/4_2_Time_Complexity_part_2/exponential_complexity_fibonacci_number/order_n_answer_saved_type_2.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+if ans saved
+Time Complexity = O(n)
+Memory Complexity = O(n)
+*/
+
+int called = 0;
+int save[100];
+
+int fib(int n)
+{
+    if(n==0)
+        return 0;
+    if(n==1)
+        return 1;
+
+    if(save[n]!=0)
+        return save[n];
+
+    //fib(n-1) + fib(n-2)
+    int x = fib(n-1);
+    called++;
+    int y = fib(n-2);
+    called++;
+    save[n] = x+y;
+    return x+y;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<"Fib = "<<fib(n)<<"\n";
+    cout<<"Called = "<<called<<"\n";
+
+    return 0;
+}
+
+/*
+n
+n-th fibonacci number?
+
+F[0] = 0
+F[1] = 1
+F[2] = F[1] + F[0] = 1
+F[3] = F[2] + F[1] = 1+1 = 2
+F[4] = F[3] + F[2] = 2+1 = 3
+F[5] = F[4] + F[3] = 3+2 = 5
+
+F[i] = F[i-1] + F[i-2]
+*/
