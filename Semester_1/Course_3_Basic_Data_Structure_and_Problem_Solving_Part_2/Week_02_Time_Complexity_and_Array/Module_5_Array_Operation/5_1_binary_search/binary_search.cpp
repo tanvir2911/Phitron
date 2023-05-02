add2953d8f050056df6d2c01d3d37d3c544f0080
Md Mozammel Hossain <hossainmozammel27@gmail.com>
2023-05-02 13:35:34 +0600

    module 5 binary search added

---
+++ Semester_1/Course_3_Basic_Data_Structure_and_Problem_Solving_Part_2/Week_02_Time_Complexity_and_Array/Module_5_Array_Operation/5_1_binary_search/binary_search.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n,k;
+    cin>>n>>k;
+    vector<int>a(n);
+    for(int i=0;i<n;i++)
+        cin>>a[i];
+
+    sort(a.begin(),a.end());
+
+    int low=0, high=n-1;
+    bool flag = 0;
+    while(low<=high)
+    {
+        int mid = (low+high)/2;
+        cout<<"Low = "<<low<<" High = "<<high<<" Mid = "<<mid<<"\n";
+        if(a[mid]==k)
+        {
+            cout<<"Yes "<<mid<<"\n";
+            flag = 1;
+            break;
+        }
+        if(k<a[mid])
+        {
+            high=mid-1;
+        }
+        else
+        {
+            low = mid+1;
+        }
+    }
+
+    if(flag==0)
+        cout<<"No\n";
+
+    return 0;
+}
+
+/*
+n
+sorted array
+k
+
+test cases:
+10 10
+2 4 6 7 8 9 10 11 14 18
+
+10 13
+2 4 6 7 8 9 10 11 14 18
+*/
